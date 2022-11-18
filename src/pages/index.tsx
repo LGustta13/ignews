92c96a18e35020d143ff76df782d26f54cae70ec
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-11-17 23:40:13 -0300

    Integrating Stripe service and getServerSIdePorps function for SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+type HomeProps = {
+  product: {
+    priceId: string,
+    amount: number
+  }
+
+}
+
+export default function Home({ product }: HomeProps) {
 
   return (
     <>
@@ -18,9 +27,9 @@ export default function Home() {
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.98 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
@@ -30,9 +39,27 @@ export default function Home() {
 
 // variável que recebe uma função que realiza tarefas no lado do servidor (deve ter exatamente este nome!)
 export const getServerSideProps: GetServerSideProps = async () => {
+
+  // Id que representa o preço do produto
+  const price = await stripe.prices.retrieve('price_1M5INcK5XDk9bXHHDVt9BPO8', {
+
+    // Utilizado para retornar todas as informações sobre o produto, segundo a documentação ele pode ser expandido
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    // Fiz isso porque o price.unit_amount retorna ou number ou null, dai preciso tratar o null por causa do TypeScript
+    // Além disso precisa-se formatar o valor para aparecer em cifrão
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+  };
+
   return {
     props: {
-      nome: 'Gustavo'
+      product,
     }
   }
 }
