a9810e0f61d9426cf0ad04728dae26e14605f8e1
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-11-21 22:14:24 -0300

    Testing API routes from Next

---
+++ src/pages/api/users.ts
@@ -0,0 +1,17 @@
+// Criei uma rota para buscar os usuários no banco de dados. Este arquivo está sendo executado no Next, o código
+// não é acessíovel pelo cliente, somente os dados retornados 
+// Request - requisição http
+// Response - retorno da requisição http, JSON
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function Users(request: NextApiRequest, response: NextApiResponse) {
+    const users = [
+        { id: 1, name: 'Diego' },
+        { id: 2, name: 'Dani' },
+        { id: 3, name: 'Rafa' },
+    ]
+
+    return response.json(users)
+
+}
