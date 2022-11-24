fe2e19d15205a5fbc0c487a5f8b773f34f5c1ae6
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-11-24 00:01:11 -0300

    signOut button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,25 +3,32 @@ import { FiX } from "react-icons/fi";
 
 // signIn - Função do next-auth que faz a autenticação do usuário
 // useSession - Hook do react que verifica se o usuário tem uma sessão ativa
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
 
-    const { data: session, status } = useSession();
+    const { data: session } = useSession();
+
+    console.log(session);
 
     function handleSignInButton() {
         signIn('github');
+    }
+
+    function handleSignOutButton() {
+        signOut();
     }
 
     return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={handleSignOutButton}
         >
             <FaGithub color="#84d363" />
-            Gustavo
+            {session.user?.name}
             <FiX color="#737388" className={styles.closeIcon} />
         </button>
     ) : (
