05ae0deaefb5066629efcefeecc67aa8660be79a
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-11-23 21:32:37 -0300

    Added signIn function and useSession for login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,13 +1,21 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
+// signIn - Função do next-auth que faz a autenticação do usuário
+// useSession - Hook do react que verifica se o usuário tem uma sessão ativa
+import { signIn, useSession } from 'next-auth/react';
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
 
-    const isUserLoggedIn = true;
+    const { data: session, status } = useSession();
 
-    return isUserLoggedIn ? (
+    function handleSignInButton() {
+        signIn('github');
+    }
+
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
@@ -20,6 +28,7 @@ export function SignInButton() {
         <button
             type="button"
             className={styles.signInButton}
+            onClick={handleSignInButton}
         >
             <FaGithub color="#eba417" />
             Sign in with Github
