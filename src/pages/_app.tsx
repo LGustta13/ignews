6e6871cb6e07fb3e370c5f0089f13a6b3b8d6468
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-11-23 21:33:12 -0300

    Added SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { SessionProvider } from 'next-auth/react';
 
 // Toda importação de estilização global deve ser feita em _app
 // Qualquer elemento que precise ser compartilhado ou persistido entre várias páginas vai em _app
@@ -7,10 +8,11 @@ import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    // Significa que qualquer reload será retornado as informações da session pelo pageProps
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
